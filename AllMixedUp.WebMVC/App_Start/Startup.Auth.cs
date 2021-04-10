d1f84dbfa3fe1e12cf53de075638df5be090926b
nikkigrostefon@gmail.com <nikkigrostefon@gmail.com>
2021-04-10 17:29:57 -0400

    using statements added

--- AllMixedUp.WebMVC/App_Start/Startup.Auth.cs
+++ AllMixedUp.WebMVC/App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using AllMixedUp.WebMVC.Models;
+using AllMixedUp.Data;
 
 namespace AllMixedUp.WebMVC
 {
--- AllMixedUp.WebMVC/Controllers/AccountController.cs
+++ AllMixedUp.WebMVC/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using AllMixedUp.WebMVC.Models;
+using AllMixedUp.Data;
 
 namespace AllMixedUp.WebMVC.Controllers
 {

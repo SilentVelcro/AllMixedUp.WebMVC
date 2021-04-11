ee195616372ac7e49a36bf3ef549f919c0dbb62e
nikkigrostefon@gmail.com <nikkigrostefon@gmail.com>
2021-04-11 17:53:40 -0400

    Min & Max Cone Enum created glaxes.data

--- AllMixedUp.Data/Glaze.cs
+++ AllMixedUp.Data/Glaze.cs
@@ -29,15 +29,89 @@ namespace AllMixedUp.Data
             SaltSoda = 5,
             Raku = 6,
         }
+        public FireProcess Atmosphere { get; set; }
 
-        //public enum MinCone
-        //{
+        public enum MinCone
+        {
+            Cone022 = 1,
+            Cone021 = 2,
+            Cone020 = 3,
+            Cone019 = 4,
+            Cone018 = 5,
+            Cone017 = 6,
+            Cone016 = 7,
+            Cone015 = 8,
+            Cone014 = 9,
+            Cone013 = 10,
+            Cone012 = 11,
+            Cone011 = 12,
+            Cone010 = 13,
+            Cone09 = 14,
+            Cone08 = 15,
+            Cone07 = 16,
+            Cone06 = 17,
+            Cone05 = 18,
+            Cone04 = 19,
+            Cone03 = 20,
+            Cone02 = 21,
+            Cone01 = 22,
+            Cone1 = 23,
+            Cone2 = 24,
+            Cone3 = 25,
+            Cone3Point5 = 26,
+            Cone4 = 27,
+            Cone5 = 28,
+            Cone6 = 29,
+            Cone7 = 30,
+            Cone8 = 31,
+            Cone9 = 32,
+            Cone10 = 33,
+            Cone11 = 34,
+            Cone12 = 35,
+            Cone13 = 36,
+        }
+        public MinCone MinimumCone { get; set; }
 
-        //}
-        //public enum MaxCone
-        //{
-
-        //}
+        public enum MaxCone
+        {
+            Cone022 = 1,
+            Cone021 = 2,
+            Cone020 = 3,
+            Cone019 = 4,
+            Cone018 = 5,
+            Cone017 = 6,
+            Cone016 = 7,
+            Cone015 = 8,
+            Cone014 = 9,
+            Cone013 = 10,
+            Cone012 = 11,
+            Cone011 = 12,
+            Cone010 = 13,
+            Cone09 = 14,
+            Cone08 = 15,
+            Cone07 = 16,
+            Cone06 = 17,
+            Cone05 = 18,
+            Cone04 = 19,
+            Cone03 = 20,
+            Cone02 = 21,
+            Cone01 = 22,
+            Cone1 = 23,
+            Cone2 = 24,
+            Cone3 = 25,
+            Cone3Point5 = 26,
+            Cone4 = 27,
+            Cone5 = 28,
+            Cone6 = 29,
+            Cone7 = 30,
+            Cone8 = 31,
+            Cone9 = 32,
+            Cone10 = 33,
+            Cone11 = 34,
+            Cone12 = 35,
+            Cone13 = 36,
+        }
+        public MaxCone MaximumCone { get; set; }
 
         //public enum MainColor
         //{

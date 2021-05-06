1cef87e9aa6bf352366d2108c461a35b53631e48
Nikki Grostefon <73565265+NikkiGrostefon@users.noreply.github.com>
2021-05-06 00:07:02 -0400

    dev to master

---
+++ AllMixedUp.Data/Migrations/202105060404369_END.cs
@@ -0,0 +1,16 @@
+unamespace AllMixedUp.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class END : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
